72d35c531d8788a7c4a78d7192ba2e00fb85c142
Christiano Pires <cristianof95@gmail.com>
2020-12-19 16:43:28 -0400

    xcproject create

---
+++ MyFavoritesTvShows/MyFavoritesTvShows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoritesTvShows
+//
+//  Created by christiano filipe pinto on 19/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
